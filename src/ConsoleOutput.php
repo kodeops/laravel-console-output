61ba5bba2f9a91b4633104662081438a4831924f
Xavier Hernandez <xavier@kodeops.io>
2022-10-21 22:09:13 +0200

    Add bugsnag breadcrumb

--- src/ConsoleOutput.php
+++ src/ConsoleOutput.php
@@ -2,45 +2,62 @@
 namespace kodeops\LaravelConsoleOutput;
 
 use Symfony\Component\Console\Output\ConsoleOutput as SConsoleOutput;
+use Bugsnag\BugsnagLaravel\Facades\Bugsnag;
+use Bugsnag\Breadcrumbs\Breadcrumb;
 
 class ConsoleOutput extends SConsoleOutput
 {
-    public function info($message)
+    private function convertMessageForConsole($message)
     {
-        if (! app()->isProduction() AND function_exists('ray')) {
-            ray($message)->color('green');
+        if (is_object($message)) {
+            $message = 'is_object';
         }
 
         if (is_array($message)) {
             $message = json_encode($message);
         }
 
+        return $message;
+    }
+
+    private function ray($message, string $color = 'blue')
+    {
+        if (! app()->isProduction() AND function_exists('ray')) {
+            ray($message)->color($color);
+        }
+    }
+
+    private function breadcrumb($message, $type)
+    {
+        if (is_array($message)) {
+            Bugsnag::leaveBreadcrumb(null, $type, $message);
+            return;
+        }
+
+        Bugsnag::leaveBreadcrumb($message, $type);
+    }
+
+    public function info($message)
+    {
+        $this->ray($message, 'green');
+        $this->breadcrumb($message, Breadcrumb::LOG_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<info>{$message}</info>");
     }
 
     public function error($message)
     {
-        if (! app()->isProduction() AND function_exists('ray')) {
-            ray($message)->color('red');
-        }
-
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-
+        $this->ray($message, 'red');
+        $this->breadcrumb($message, Breadcrumb::ERROR_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<error>{$message}</error>");
     }
 
     public function comment($message)
     {
-        if (! app()->isProduction() AND function_exists('ray')) {
-            ray($message);
-        }
-
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-
+        $this->ray($message);
+        $this->breadcrumb($message, Breadcrumb::LOG_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<comment>{$message}</comment>");
     }
 
@@ -59,14 +76,9 @@ class ConsoleOutput extends SConsoleOutput
             return;
         }
 
-        if (function_exists('ray')) {
-            ray($message);
-        }
-
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-        
+        $this->ray($message);
+        $this->breadcrumb($message, Breadcrumb::LOG_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<comment>{$message}</comment>");
     }
 
@@ -76,14 +88,9 @@ class ConsoleOutput extends SConsoleOutput
             return;
         }
 
-        if (function_exists('ray')) {
-            ray($message)->color('green');
-        }
-
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-
+        $this->ray($message, 'green');
+        $this->breadcrumb($message, Breadcrumb::LOG_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<info>{$message}</info>");
     }
 
@@ -93,14 +100,9 @@ class ConsoleOutput extends SConsoleOutput
             return;
         }
 
-        if (function_exists('ray')) {
-            ray($message)->color('error');
-        }
-
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-
+        $this->ray($message, 'red');
+        $this->breadcrumb($message, Breadcrumb::ERROR_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<error>{$message}</error>");
     }
 
@@ -110,14 +112,9 @@ class ConsoleOutput extends SConsoleOutput
             return;
         }
 
-        if (function_exists('ray')) {
-            ray($message);
-        }
-
-        if (is_array($message)) {
-            $message = json_encode($message);
-        }
-
+        $this->ray($message);
+        $this->breadcrumb($message, Breadcrumb::LOG_TYPE);
+        $message = $this->convertMessageForConsole($message);
         return $this->writeln("<comment>{$message}</comment>");
     }
 }
